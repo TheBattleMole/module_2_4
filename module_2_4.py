6060f7ab68aa136b381a3dc3c89aaf08ee1a35fc
Aleksey_Abramenko <x903xxxx@gmail.com>
2024-11-28 13:15:07 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,16 +1,17 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-k = 0 #делители
-for i in range(2, 16):
-    for j in range (2, i):
+for i in numbers:
+    if i == 1:
+        continue
+    prime = True
+    for j in range(2,i):
         if i % j == 0:
-            not_primes.append(i)
-            k += 1
-    if k == 0:
+            prime = False
+            break
+    if prime:
         primes.append(i)
     else:
-        k = 0
+        not_primes.append(i)
 print(primes)
-not_primes_ = list(set(not_primes))
-print(not_primes_)
+print(not_primes)
